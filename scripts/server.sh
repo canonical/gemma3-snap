5b16a076e09d80ee464b7e2d72d478a136b462a2
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2025-12-03 12:02:28 +0100

    Revert "Use jq for modelctl output processing"
    This reverts commit 22f42f2c3e43acb103c1d3ebb5c719cd8feda2ae.

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,10 +1,9 @@
 #!/bin/bash -eu
 
-engine="$(modelctl show-engine --format=json | jq .name)"
-
 # Save the status for content sharing
 # This must be done each time the server is started to expose the actual status
 mkdir -p "$STATUS_SHARE"
 modelctl status --format=json > "$STATUS_SHARE/status.json"
 
+engine="$(modelctl show-engine | yq .name)"
 modelctl run "$SNAP/engines/$engine/server" --wait-for-components
