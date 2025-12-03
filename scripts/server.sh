22f42f2c3e43acb103c1d3ebb5c719cd8feda2ae
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2025-12-03 11:26:51 +0100

    Use jq for modelctl output processing

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -eu
 
-engine="$(modelctl show-engine | yq .name)"
+engine="$(modelctl show-engine --format=json | jq .name)"
+
 modelctl run "$SNAP/engines/$engine/server" --wait-for-components
