6c49b886bc509d169b0df703bafbbfa40632d6b2
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2025-12-03 11:27:53 +0100

    Add content slot to share status

--- scripts/server.sh
+++ scripts/server.sh
@@ -2,4 +2,9 @@
 
 engine="$(modelctl show-engine --format=json | jq .name)"
 
+# Save the status for content sharing
+# This must be done each time the server is started to expose the actual status
+mkdir -p "$STATUS_SHARE"
+modelctl status --format=json > "$STATUS_SHARE/status.json"
+
 modelctl run "$SNAP/engines/$engine/server" --wait-for-components
