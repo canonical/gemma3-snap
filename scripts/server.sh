63fcf408c6e4ef0a832c60ee42be91dc05a7013e
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2026-04-02 13:01:43 +0200

    Wait for components with status command

--- scripts/server.sh
+++ scripts/server.sh
@@ -3,9 +3,7 @@
 # Save the status for content sharing
 # This must be done each time the server is started to expose the actual status
 mkdir -p "$STATUS_SHARE"
-# Wait for component before querying status
-modelctl run true --wait-for-components && \
-    modelctl status --format=json > "$STATUS_SHARE/status.json"
+modelctl status --wait-for-components --format=json > "$STATUS_SHARE/status.json"
 
 engine="$(modelctl show-engine --format=json | jq -r .name)"
 modelctl run "$SNAP/engines/$engine/server" --wait-for-components
