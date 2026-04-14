487891c55a9972dae47e98375877b06fabf851b8
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2026-04-14 18:12:26 +0200

    Rename script to export-shared-configs.sh

--- scripts/server.sh
+++ scripts/server.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 # Export the configuration for content sharing
 # This must be done each time the server is started to expose the actual configuration
-$SNAP/bin/write-configs.sh
+$SNAP/bin/export-shared-configs.sh
 
 engine="$(modelctl status --wait-for-components --format=json | jq -r .engine)"
 exec modelctl run "$SNAP/engines/$engine/server" --wait-for-components
