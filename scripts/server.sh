667e6b9a974d75b82b6f3d49191d22bb68e41ef9
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2026-06-23 10:43:32 +0200

    Add missing wait to status

--- scripts/server.sh
+++ scripts/server.sh
@@ -6,5 +6,5 @@ set -euo pipefail
 # This must be done each time the server is started to expose the actual configuration
 $SNAP/bin/export-shared-configs.sh
 
-engine="$(modelctl status --format=json | jq -r .engine)"
+engine="$(modelctl status --wait-for-components --format=json | jq -r .engine)"
 exec modelctl run -- "$SNAP/engines/$engine/server" "$@"
