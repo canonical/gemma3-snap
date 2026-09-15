e8e72b1a6bb4080a240aa54ec57261416cdcb203
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2026-09-15 11:45:53 +0000

    Switch to use v2 provider sharing
    Use the provider sharing mechanism added by
    https://github.com/canonical/inference-snaps-cli/pull/432 to CLI
    v2.0.0-beta.14.
    
    Remove deprecated status and open-webui interfaces.
    
    Replicates canonical/gemma4-snap#122.

--- scripts/server.sh
+++ scripts/server.sh
@@ -2,9 +2,5 @@
 
 set -euo pipefail
 
-# Export the configuration for content sharing
-# This must be done each time the server is started to expose the actual configuration
-$SNAP/bin/export-shared-configs.sh
-
 engine="$(modelctl status --wait-for-components --format=json | jq -r .engine)"
-exec modelctl run -- "$SNAP/engines/$engine/server" "$@"
+exec modelctl run --share-provider -- "$SNAP/engines/$engine/server" "$@"
